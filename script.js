f0c3fa9e5f024af5debe41fa3ae3aa3ded6a8ef5
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-22 21:21:09 -0800

    commit w/timer and beginning of nested for loop

--- script.js
+++ script.js
@@ -2,9 +2,35 @@
 
 var startBtn = document.getElementById("startBtn");
 var timeLeft = document.querySelector(".timeLeft");
-var questions = document.startBtn.addEventListener("click", function() {
+startBtn.addEventListener("click", function() {
   startQuiz();
 });
+var questionsArr = [
+  {
+    title: "What method starts a timer in Javascript?",
+    choices: ["Function", "setInterval", "clearinterval"],
+    answer: "setInerval"
+  },
+  {
+    title: "Which symbol is associated with an ID in CSS?",
+    choices: ["#", "$", "."],
+    answer: "#"
+  },
+  {
+    title: "What does API(in terms of Web Development) stand for?",
+    choices: [
+      "Advanced Placement Index",
+      "Application Programing Interfacing",
+      "Alpha Pi Iota"
+    ],
+    answer: "Application Programming Interfacing"
+  },
+  {
+    title: "Which symbol is associated with an ID in CSS?",
+    choices: ["#", "$", "."],
+    answer: "#"
+  }
+];
 
 // Universal variables
 var totalSeconds = 60;
@@ -15,10 +41,21 @@ function startQuiz() {
     totalSeconds--;
     timeLeft.textContent = "Time Left: " + totalSeconds;
 
+    function stopClock() {
+      clearTimeout(startQuiz);
+    }
+
     if (totalSeconds === 0) {
       clearInterval(startQuiz);
     }
   }, 1000);
+
+  for (i = 0; i < questionsArr.length; i++) {
+    console.log(questionsArr[i].title);
+    for (j = 0; j < questionsArr[i].choices.length; j++) {
+      console.log(questionsArr[i].choices[j]);
+    }
+  }
 }
 
 // Next steps:
