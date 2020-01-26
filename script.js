86a7633ebbc116eb1cfbc1c89c5ca26b9a26f0f3
Nicholas Jaquez <nrjaquez@gmail.com>
2020-01-25 22:08:08 -0800

    commit with questions and some choices displaying

--- script.js
+++ script.js
@@ -5,9 +5,7 @@ var timeLeft = document.querySelector(".timeLeft");
 var pointsEarned = document.querySelector(".Score");
 startBtn.addEventListener("click", function() {
   startQuiz();
-  startBtn.addEventListener("click", function() {
-    startScore();
-  });
+  // startScore();
 });
 // startBtn.addEventListener("click", function() {
 //   showQuestions();
@@ -18,17 +16,17 @@ startBtn.addEventListener("click", function() {
 
 var questionsArr = [
   {
-    prompt: "What method starts a timer in Javascript?",
+    question: "What method starts a timer in Javascript?",
     choices: ["Function", "setInterval", "clearinterval"],
     answer: "setInerval"
   },
   {
-    prompt: "Which symbol is associated with an ID in CSS?",
+    question: "Which symbol is associated with an ID in CSS?",
     choices: ["#", "$", "."],
     answer: "#"
   },
   {
-    prompt: "What does API(in terms of Web Development) stand for?",
+    question: "What does API(in terms of Web Development) stand for?",
     choices: [
       "Advanced Placement Index",
       "Application Programing Interfacing",
@@ -37,9 +35,9 @@ var questionsArr = [
     answer: "Application Programming Interfacing"
   },
   {
-    prompt: "Which symbol is associated with an ID in CSS?",
-    choices: ["#", "$", "."],
-    answer: "#"
+    question: "What helps give style to a website?",
+    choices: ["CSS", "HTML", "JavaScript"],
+    answer: "CSS"
   }
 ];
 
@@ -60,25 +58,28 @@ function startQuiz() {
   // Create a function that will show the questions on the page
 
   for (i = 0; i < questionsArr.length; i++) {
-    var answers = window.prompt(questionsArr[i].prompt);
-    if (answer == questionsArr[i].answer) {
-      score++;
-      alert("Correct!");
-    } else {
-      alert("Incorrect!");
-    }
-    if (answer == questionsArr[j].answer) {
-      score++;
-      alert("Correct!");
-    } else {
-      alert("Incorrect!");
-    }
-    if (answer == questionsArr[k].answer) {
-      score++;
-      alert("Correct!");
-    } else {
-      alert("Incorrect!");
-    }
+    //Grab the question
+    var questionString = questionsArr[i].question;
+    // Show the user the question
+
+    var displayQuestion = document.getElementById("questions");
+    displayQuestion.innerHTML = questionString;
+
+    // Grab the choices
+    var choicesString = questionsArr[i].choices[0];
+    var choicesString = questionsArr[i].choices[1];
+    var choicesString = questionsArr[i].choices[2];
+
+    // // Show the user the choices
+    var displayChoices = document.getElementById("choiceA");
+    displayChoices.innerHTML = choicesString;
+
+    var displayChoices = document.getElementById("choiceB");
+    displayChoices.innerHTML = choicesString;
+
+    var displayChoices = document.getElementById("choiceC");
+    displayChoices.innerHTML = choicesString;
+
     // console.log(questionsArr[i].title);
     for (j = 0; j < questionsArr[i].choices.length; j++) {
       // console.log(questionsArr[i].choices[j]);
@@ -89,15 +90,15 @@ function startQuiz() {
   }
 }
 
-var div = document.createElement("card");
-div.setAttribute("#questions", questionsArr);
+// var div = document.createElement("card");
+// div.setAttribute("#questions", questionsArr);
 
-document.appendChild("questions");
+// document.appendChild("questions");
 
-// Create a function to take score
-function startScore() {
-  var scoreinterval = setInterval(function() {
-    totalPoints--;
-    Score.textContent = "Points Earned: ";
-  });
-}
+// // Create a function to take score
+// function startScore() {
+//   var scoreinterval = setInterval(function() {
+//     totalPoints--;
+//     Score.textContent = "Points Earned: ";
+//   });
+// }
